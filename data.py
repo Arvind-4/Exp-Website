a90738e218bd1b5cbc76ef45f5fec59a55f6bc94
Arvind-4 <lordarvindthegamer@gmail.com>
2022-12-10 17:33:31 +0530

    Added: fake data generation

---
+++ data.py
@@ -0,0 +1,32 @@
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+
+import django
+
+django.setup()
+
+from django.db import transaction
+from faker import Faker
+from content.models import Content, Category, SubjectList
+
+fake = Faker()
+
+
+@transaction.atomic
+def create_subjects():
+    models = [Content, Category, SubjectList]
+    for m in models:
+        m.objects.all().delete()
+    for k in range(1000):
+        content = Content.objects.create(
+            title=fake.name(),
+            content=fake.text(),
+            subject=SubjectList.objects.create(subject=fake.name()),
+        )
+        content.save()
+        Category.objects.create(category=fake.name())
+    return True
+
+
+create_subjects()
