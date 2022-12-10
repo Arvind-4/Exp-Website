f24d20fe770ec69e2e78c423c2ed33121ace01c8
Arvind-4 <lordarvindthegamer@gmail.com>
2022-12-10 18:24:48 +0530

    Updated: view.py bugs

--- content/views.py
+++ content/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpRequest, HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db import transaction
 
 # Create your views here.
 
@@ -25,9 +27,17 @@ def content_likes(request, slug):
         content.likes.add(request.user)
     return HttpResponseRedirect(reverse("content-detail", args=[str(slug)]))
 
-
+@transaction.atomic
 def index(request: HttpRequest):
-    l = list(Content.objects.all())
+    l_ = tuple(Content.objects.values('title', 'slug'))
+    paginator = Paginator(l_, 10)
+    page = request.GET.get('page', 1)
+    try:
+        l = paginator.page(page)
+    except PageNotAnInteger:
+        l = paginator.page(1)
+    except EmptyPage:
+        l = paginator.page(paginator.num_pages)
     context = {"object_list": l}
     template = "content/home.html"
     return render(request, template, context)
@@ -36,13 +46,17 @@ def index(request: HttpRequest):
 def detail(request: HttpRequest, slug: str):
     liked = False
     l = get_object_or_404(Content, slug=slug)
-    liked = l.likes.filter(email__iexact=request.user.email).exists()
-    ip = get_client_ip(request)
-    if IpAddress.objects.filter(ip=ip).exists():
-        l.views.add(IpAddress.objects.get(ip=ip))
-    else:
-        IpAddress.objects.create(ip=ip)
-        l.views.add(IpAddress.objects.get(ip=ip))
+    try:
+        liked = l.likes.filter(email__iexact=request.user.email).exists()
+        ip = get_client_ip(request)
+        obj = IpAddress.objects.filter(ip=ip)
+        if obj.exists():
+            l.views.add(obj)
+        else:
+            obj_new = IpAddress.objects.create(ip=ip)
+            l.views.add(obj_new)
+    except:
+        pass
     context = {"object": l, "content_is_liked": liked}
     template = "content/detail.html"
     return render(request, template, context)
